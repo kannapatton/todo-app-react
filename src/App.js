4b785c77020c10b4fd6714b5ca009850ec175d4f
kannapatton <kandirbrts@gmail.com>
2022-07-19 21:32:49 -0500

    done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,52 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'; 
+//import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      isClicked: true, 
+      inputValue: "", 
+      listOfTodos: []
+    }
+  }
+
+  handleClick = () => {
+    this.state.isClicked ? 
+    this.setState({isClicked : false}) : 
+    this.setState({isClicked : true})
+  }
+
+  handleChange = (event) => {
+    this.setState({inputValue: event.target.value})
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    this.setState({listOfTodos: [...this.state.listOfTodos, this.state.inputValue]})
+    this.setState({inputValue: ""})
+  }
+
+  render() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <form onSubmit={this.handleSubmit}>
+          <input type= "text" value = {this.state.inputValue} onChange={this.handleChange}>
+          </input>
+          <button type="submit">Submit</button>
+        </form>
+        <ol>
+          {this.state.listOfTodos.map((todo, index) =>{
+            return <li key={index}>{todo}</li>
+          })}
+        </ol>
+        <button onClick = {this.handleClick}>Click Me</button>
         <p>
-          Kandi
+          {this.state.isClicked ? "true" : "false"}
         </p>
         <a
           className="App-link"
@@ -20,6 +59,7 @@ function App() {
       </header>
     </div>
   );
-}
+  };
+};
 
 export default App;
